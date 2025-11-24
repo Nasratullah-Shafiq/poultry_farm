68e0e78940f2d791e1d20c71b6c6be114829267a
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-11-24 12:05:59 +0430

    new changes

--- models/payment.py
+++ models/payment.py
@@ -20,19 +20,56 @@ class PoultryPayment(models.Model):
         store=False
     )
 
+    # @api.depends('customer_id')
+    # def _compute_amount_due(self):
+    #     for rec in self:
+    #         if rec.customer_id:
+    #             # Get all unpaid sales of this customer
+    #             sales = self.env['poultry.sale'].search([
+    #                 ('customer_id', '=', rec.customer_id.id)
+    #             ])
+    #
+    #             # Sum total due
+    #             total_due = sum(sale.amount_due for sale in sales)
+    #
+    #             rec.amount_due = total_due
+    #         else:
+    #             rec.amount_due = 0
+    #
+    # @api.model
+    # def create(self, vals):
+    #     payment = super(PoultryPayment, self).create(vals)
+    #     sale = payment.sale_id
+    #     if sale:
+    #         # Add payment to sale
+    #         sale.amount_received += payment.amount
+    #         # Update payment status automatically
+    #         if sale.amount_received >= sale.revenue:
+    #             sale.payment_status = 'paid'
+    #         elif sale.amount_received > 0:
+    #             sale.payment_status = 'partial'
+    #         else:
+    #             sale.payment_status = 'not_paid'
+    #     return payment
+
     @api.depends('customer_id')
     def _compute_amount_due(self):
         for rec in self:
             if rec.customer_id:
-                # Get all unpaid sales of this customer
+                # Get all sales of this customer
                 sales = self.env['poultry.sale'].search([
                     ('customer_id', '=', rec.customer_id.id)
                 ])
+                total_sales = sum(sale.amount for sale in sales)
 
-                # Sum total due
-                total_due = sum(sale.amount_due for sale in sales)
+                # Get all payments made by this customer
+                payments = self.env['poultry.payment'].search([
+                    ('customer_id', '=', rec.customer_id.id)
+                ])
+                total_paid = sum(payment.amount for payment in payments)
 
-                rec.amount_due = total_due
+                # Compute total due
+                rec.amount_due = total_sales - total_paid
             else:
                 rec.amount_due = 0
 
@@ -41,13 +78,19 @@ class PoultryPayment(models.Model):
         payment = super(PoultryPayment, self).create(vals)
         sale = payment.sale_id
         if sale:
-            # Add payment to sale
-            sale.amount_received += payment.amount
-            # Update payment status automatically
-            if sale.amount_received >= sale.revenue:
-                sale.payment_status = 'paid'
-            elif sale.amount_received > 0:
-                sale.payment_status = 'partial'
-            else:
-                sale.payment_status = 'not_paid'
+            # Optional: Update sale's amount_received field if you have it
+            if hasattr(sale, 'amount_received'):
+                sale.amount_received = sum(
+                    self.env['poultry.payment'].search([('sale_id', '=', sale.id)]).mapped('amount')
+                )
+
+            # Optional: Update payment status if you have a field
+            if hasattr(sale, 'payment_status') and hasattr(sale, 'revenue'):
+                if sale.amount_received >= sale.revenue:
+                    sale.payment_status = 'paid'
+                elif sale.amount_received > 0:
+                    sale.payment_status = 'partial'
+                else:
+                    sale.payment_status = 'not_paid'
+
         return payment
