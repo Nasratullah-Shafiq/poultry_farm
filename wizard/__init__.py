d2613f09b5268ee6fcb964ccfa280074d6bf61ca
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-01-07 08:48:44 +0430

    new changes

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,5 +1,1 @@
-from . import finance_report_wizard
-from . import sale_report_wizard
-from . import purchase_report_wizard
-from . import salary_payment_wizard
-from . import expense_report_wizard
+from . import models
