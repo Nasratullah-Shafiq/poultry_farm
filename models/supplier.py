b64cceb46abe83aaadaae0cddcfefbf3a9c904a9
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-01-09 09:57:55 +0430

    Remove commented-out code from SupplierPayment model for cleaner implementation

--- models/supplier.py
+++ models/supplier.py
@@ -303,17 +303,3 @@ class SupplierPayment(models.Model):
 
 
 
-
-# from odoo import models, fields
-#
-# class PoultrySupplier(models.Model):
-#     _name = 'poultry.supplier'
-#     _description = 'Poultry Supplier'
-#
-#     name = fields.Char(string='Supplier Name', required=True)
-#     phone = fields.Char(string='Phone')
-#     email = fields.Char(string='Email')
-#     address = fields.Text(string='Address')
-#
-#     # Optional: show linked purchases in supplier form
-#     purchase_ids = fields.One2many('poultry.purchase', 'supplier_id', string='Purchases')
