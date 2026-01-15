46dfee91bf1153e4a48fd62d4f4b1cf6f6df0b78
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-01-15 12:00:27 +0430

    Fix import order in __init__.py for report wizards

--- wizard/models/__init__.py
+++ wizard/models/__init__.py
@@ -3,3 +3,6 @@ from . import sale_report_wizard
 from . import purchase_report_wizard
 from . import salary_payment_wizard
 from . import expense_report_wizard
+from . import death_report_wizard
+from . import cash_deposit_report_wizard
+from . import cash_account_report_wizard
